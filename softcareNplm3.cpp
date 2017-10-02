089b854ea9a16f58e4ca967bca193852cb156669
Suharto <suvro.suharto@gmail.com>
2017-10-03 00:02:08 +0600

    Write a C program to print a big 'C' .

---
+++ softcareNplm3.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j;
+
+    for (i=1;i<=3;i++)
+    {
+     printf(" ");
+    }
+     for ( a=1;a<=6;a++)
+     {
+      printf("#");
+     }
+     for ( a=1;a<2;a++)
+     {
+         printf("\n ");
+     }
+     for ( a=1;a<=2;a++)
+     {
+         printf("#");
+     }
+     for ( a=1;a<=6;a++)
+     {
+      printf(" ");
+     }
+     for ( a=1;a<=2;a++)
+     {
+         printf("#");
+     }
+     for ( a=1;a<=6;a++)
+     {
+      printf("\n#");
+     }
+      for ( a=1;a<2;a++)
+     {
+         printf("\n ");
+     }
+      for ( a=1;a<=2;a++)
+     {
+      printf("#");
+     }
+       for ( a=1;a<=6;a++)
+     {
+         printf(" ");
+     }
+       for ( a=1;a<=2;a++)
+     {
+         printf("#");
+     }
+     for (i=1;i<2;i++)
+    {
+     printf("\n   ");
+    }
+     for ( a=1;a<=6;a++)
+     {
+      printf("#");
+     }
+}
