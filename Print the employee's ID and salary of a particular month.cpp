2751cce41ca2f33ffa6edb32e8da6ca5d348a162
Suharto <suvro.suharto@gmail.com>
2017-10-03 22:29:09 +0600

    Print the employee's ID and salary of a particular month

---
+++ Print the employee's ID and salary of a particular month.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+	char i[10];
+	int hour;
+	double value, salary;
+	printf("Input the Employees ID(Max. 10 chars): ");
+
+	scanf("%s", &i);
+
+	printf("\nInput the working hrs: ");
+	scanf("%d", &hour);
+
+	printf("\nSalary amount/hr: ");
+
+	scanf("%lf", &value);
+
+	salary = value * hour;
+	printf("\nEmployees ID = %s\nSalary = U$ %.2lf\n", i,salary);
+	return 0;
+}
